1462498f55b2080474270d617b1842a066eda946
admin <admin@admins-MacBook-Pro.local>
2021-01-03 16:52:40 -0500

    added media queries

---
+++ src/components/title/title.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './title.styles.scss';
+
+const Title = () => (
+	<div className='title'>
+	Being a cool cat made easy
+	</div>
+);
+
+export default Title;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Directory from '../../components/directory/directory.component';
+import Directory from '../../components/directory/directory.component.jsx';
+import Title from '../../components/title/title.component.jsx';
 
-import './homepage.styles.scss'
+import './homepage.styles.scss';
 
 const HomePage = ({ history }) => {
 	return (
 		<div className='homepage'>
+			<Title />
 			<Directory />
 		</div>
 
